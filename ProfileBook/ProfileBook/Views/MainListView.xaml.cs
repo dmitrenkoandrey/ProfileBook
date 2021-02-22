eaa00a67903d9da7e044095ba9af65ac70d520be
Andrey <dmitrenko61@gmail.com>
2021-02-22 11:10:45 +0200

    Add images to Project

--- ProfileBook/ProfileBook/Views/MainListView.xaml.cs
+++ ProfileBook/ProfileBook/Views/MainListView.xaml.cs
@@ -13,12 +13,9 @@ using ProfileBook.ViewModels;
 
 namespace ProfileBook.Views
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainListView : ContentPage
     {
-
         public MainListView()
         {
             InitializeComponent();
